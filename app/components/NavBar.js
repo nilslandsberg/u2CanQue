8b95ae2a14eeffd475975239a189657fbd901390
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2023-11-20 11:34:27 -0500

    initial commit. NavBar started

---
+++ app/components/NavBar.js
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import React from 'react'
+
+
+const NavBar = () => {
+  return (
+    <nav className={"bg-slate-600 dar:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600"}>
+     
+      <div className="max-w-7xl mx-auto py-5 px-3  text-white">
+        <div className="flex items-center justify-between">
+          <div>
+            <div>U 2 Can Cue, LLC</div>
+          </div>
+          <div className="flex items-center space-x-5 justify-between">
+            <a className=" hover:text-gray-400" href="#">Home</a>
+            <a className=" hover:text-gray-400" href="#">About Us</a>
+            <a className=" hover:text-gray-400" href="#">Menus</a>
+            <a className=" hover:text-gray-400" href="#">Order Online</a>
+            <a className=" hover:text-gray-400" href="#">Gift Certificates</a>
+            <a className=" hover:text-gray-400" href="#">Services</a>
+            <a className=" hover:text-gray-400" href="#">Rate Us</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar

3ced031fb97bc98224a66995ada8eab6b6770203
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2024-01-16 15:31:28 -0500

    error message added to StartOrderButton if no date is selected

--- app/components/StartOrderButton.js
+++ app/components/StartOrderButton.js
@@ -1,17 +1,23 @@
 "use client";
 
 import { useRouter } from 'next/navigation';
-import React from 'react'
+import React, { useState } from 'react'
 
 const StartOrderButton = ({ selectedDate }) => {
   const router = useRouter();
+  const [ errorMessage, setErrorMessage ] = useState()
 
   const handleClick = () => {
-    
+    console.log(selectedDate)
+    if (selectedDate === undefined) {
+      setErrorMessage("You must select a date to start your order");
+      return;
+    }
+
     const date = new Date(selectedDate);
     const dayOfWeekNumber = date.getUTCDay();
     const daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
-    
+
     // Check if day of week is a Monday - Friday and create shopping cart in local storage
     if (dayOfWeekNumber >= 1 && dayOfWeekNumber <= 5) {
       const dayOfWeek = daysOfWeek[dayOfWeekNumber];
@@ -31,7 +37,13 @@ const StartOrderButton = ({ selectedDate }) => {
   };
   
   return (
-    <button className="z-30 py-2 px-3 bg-orange-600 rounded text-gray-900 hover:bg-orange-400 hover:text-white transition duration-300" onClick={handleClick}>Start Order</button>
+    <>
+      <button className="z-30 py-2 px-3 bg-orange-600 rounded text-gray-900 hover:bg-orange-400 hover:text-white transition duration-300" onClick={handleClick}>Start Order</button>
+      {errorMessage && (
+        <p className="text-orange-600 mt-2">{errorMessage}</p>
+      )}
+    </>
+   
   )
 }
 

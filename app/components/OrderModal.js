55d5e9d56548b1f053541fe4bafc98183b95920d
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2024-01-17 13:55:59 -0500

    minor additions to the modal content.

--- app/components/OrderModal.js
+++ app/components/OrderModal.js
@@ -1,30 +1,42 @@
 "use client";
 
-import React from 'react'
+import React from 'react';
 import { useModal } from '../contexts/ModalContext';
+import Image from 'next/image';
 
 const OrderModal = () => {
   const { modalItem, closeModal } = useModal();
+
   return (
     <div className="fixed inset-0 overflow-y-auto">
-    <div className="flex items-center justify-center min-h-screen">
-      {/* Modal Overlay */}
-      <div className="fixed inset-0 bg-black bg-opacity-40"></div>
+      <div className="flex items-center justify-center min-h-screen">
+        {/* Modal Overlay */}
+        <div className="fixed inset-0 bg-black bg-opacity-10"></div>
 
-      {/* Modal Content */}
-      <div className="bg-white p-4 rounded shadow-lg z-50 relative">
-        {/* Your modal content goes here */}
-        <p className="text-gray-800">{modalItem.name}</p>
-        <button
-          onClick={() => closeModal()}
-          className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none"
-        >
-          Close Modal
-        </button>
+        {/* Modal Content */}
+        <div className="bg-black border p-4 shadow-lg flex flex-col items-center">
+          {/* Your modal content goes here */}
+
+          <p className="text-white text-2xl mt-4">{modalItem.name}</p>
+          <div className="flex flex-row mt-4">
+            <button
+              onClick={() => closeModal()}
+              className="z-30 py-2 px-3 bg-orange-600 rounded text-gray-900 hover:bg-orange-400 hover:text-white transition duration-300 mr-2"
+            >
+              Return to Menu
+            </button>
+            <button 
+              className="z-30 py-2 px-3 bg-orange-600 rounded text-gray-900 hover:bg-orange-400 hover:text-white transition duration-300"
+              onClick={() => closeModal()}
+            >
+              Add To Cart
+            </button>
+          </div>
+         
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default OrderModal
+export default OrderModal;

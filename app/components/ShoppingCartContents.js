58c1437a0bc08cf1da72fc309a31ae08b08e717d
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2024-01-25 18:53:08 -0500

    minor changes. AddToCartButton changed to CSR.

---
+++ app/components/ShoppingCartContents.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShoppingCartContents = () => {
+  return (
+    <div className="text-white">ShoppingCartContents</div>
+  )
+}
+
+export default ShoppingCartContents
--- app/order/cart/page.js
+++ app/order/cart/page.js
@@ -1,14 +1,20 @@
+import ShoppingCartContents from '@/app/components/ShoppingCartContents'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className="relative flex flex-col items-center pt-[75px] bg-black pb-4">
-      <div className="w-1/2 md:w-1/3 z-20 items-center text-white text-center">
-        <br />
-        <br />
-        <p className="text-2xl font-bold italic">Shopping Cart</p>
+    <>
+      <div className="bg-black text-white fixed top-0 left-0 right-0 z-30 pt-[90px]">
+        <div className='text-2xl font-bold italic text-center py-2 divide-white'>
+          <p>Cart</p>
+        </div>
       </div>
-    </div>
+      <div className="relative flex flex-col items-center pt-[175px] min-h-screen bg-black">
+          <div className="divide-y-2 divide-double divide-white w-full">
+            <ShoppingCartContents />
+          </div>
+        </div>
+    </>
   )
 }
 

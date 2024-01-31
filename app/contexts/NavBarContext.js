cd7e48debf6cde97d3aabcf8cc7c7e79806a62b0
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2024-01-30 22:01:06 -0500

    useEffect in NavBarContext upated to remove shoppingCart from localStorage if the date is an empty string or if the the date is tomorrow and it is after 5:30pm today

--- app/contexts/NavBarContext.js
+++ app/contexts/NavBarContext.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { DateTime } from 'luxon';
 import React, { createContext, useContext, useEffect, useState } from 'react';
 
 const NavBarContext = createContext();
@@ -11,9 +12,28 @@ export const NavBarContextProvider = ({ children }) => {
   useEffect(() => {
     // Access localStorage after component mounts
     const cart = JSON.parse(localStorage.getItem('shoppingCart'));
+    if (cart && cart.date === "") {
+      localStorage.removeItem('shoppingCart');
+      return;
+    }
+ 
+    if (cart && cart.date) {
+      const cartDate = DateTime.fromISO(cart.date);
+      const currentDateTime = DateTime.now();
+
+      const isTomorrow = currentDateTime.plus({ days: 1 }).toISODate() === cartDate.toISODate();
+
+      const isAfter530pm = currentDateTime.hour >= 17 && currentDateTime.minute >= 30;
+
+      if (isTomorrow && isAfter530pm) {
+        localStorage.removeItem('shoppingCart');
+      }
+      return;
+    }
+    
     setShoppingCart(cart);
   }, []);
-  
+
   return (
     <NavBarContext.Provider value={{ isToggled, setIsToggled, shoppingCart }}>
       {children}

29d9fe3062f5a857182740240007c0e0f7085115
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2024-04-10 16:40:11 -0400

    HolidayLink.js changed to a dropdown menu. NavBarLinks.js passes styling as props to HolidayLinks.js. react-icons installed.

--- app/components/HolidayLink.js
+++ app/components/HolidayLink.js
@@ -1,53 +1,50 @@
 import React from 'react'
 import { christmasDateCheck, easterDateCheck, memorialDayDateCheck, saleEndMessage, thanksgivingDateCheck } from '../utils/dateCheck';
+import { IoIosArrowDown } from "react-icons/io";
+import { capitalizeFirstLetter } from '../utils/stringManipulation';
 
 const HolidayLink = ({ className }) => {
   const christmasSale = christmasDateCheck();
-  
+
+  let holiday="";
+
   if (christmasSale) {
-    return (
-      <>
-        <a className={className} href="/order/christmas">Christmas Specials</a>
-        <a className={className} href="/order/christmas/cart">View Christmas Cart</a>
-      </>
-    )
+    holiday="christmas"
   } 
   
   const easterSale = easterDateCheck();
 
   if (easterSale.isItEaster) {
-    return (
-      <>
-        <a className={className} href="/order/easter">Easter Specials</a>
-        <a className={className} href="/order/easter/cart">View Easter Cart</a>
-      </>
-    )
+    holiday="easter"
   }
   
   const thanksgivingSale = thanksgivingDateCheck()
 
   if (thanksgivingSale.isItThanksgiving) {
-    return (
-      <>
-        <a className={className} href="/order/thanksgiving">Thanksgiving Specials</a>
-        <a className={className} href="/order/thanksgiving/cart">View Thanksgiving Cart</a>
-      </>
-    )
+    holiday="thanksgiving"
   }
 
   const memorialDaySale = memorialDayDateCheck();
 
   if (memorialDaySale.isItMemorialDay) {
-    return (
-      <>
-        <a className={className} href="/order/memorial-day">Memorial Day Specials</a>
-        <a className={className} href="/order/memorial-day/cart">View Memorial Day Cart</a>
-      </>     
-    )
+    holiday="memorial-day"
   }
 
   return (
-    <></>
+    <>
+      {holiday && (
+        <div className={`relative group px-2 py-3 transition-all ${className ? className : ""}`}>
+          <div className="flex cursor-pointer items-center gap-2 group-hover:text-gray-400">
+            <span>Holiday Orders</span>
+            <IoIosArrowDown className="rotate-180 transition-all group-hover:rotate-0" />
+          </div>
+          <div className="absolute left-0 top-10 hidden w-auto flex-col gap-1 rounded-xl bg-white py-3 shadow-md transition-all group-hover:flex text-black">
+            <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-2" href={`/order/${holiday}`}>{capitalizeFirstLetter(holiday)} Specials</a>
+            <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-2" href={`/order/${holiday}/cart`}>{capitalizeFirstLetter(holiday)} Cart</a>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 

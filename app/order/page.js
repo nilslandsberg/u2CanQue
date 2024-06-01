38a71c9aa076b57a81a72799f9a9f702e08d60eb
Nils Landsberg <111305349+nilslandsberg@users.noreply.github.com>
2024-06-01 07:58:15 -0500

    Update page.js
    Updated closed message.

--- app/order/page.js
+++ app/order/page.js
@@ -18,7 +18,7 @@ export default function Menu() {
           <p className="text-xl font-bold italic">Our menu changes daily.</p>
           <p className="text-xl font-bold italic"> Select a date on the calendar to explore our menu.</p>
           <br />
-          <p className="text-xl font-bold">We will not be available for orders during the week of June 10th through the 14th.  We will resume kitchen operations on June 17th.  We appreciate your understanding!</p>
+          <p className="text-xl font-bold">We will be closed during the week of June 10th through the 14th.  We will resume kitchen and store operations on June 17th.  We appreciate your understanding and look forward to meeting all of you bbq needs!</p>
           <OrderCalendar />
         </div>
         {/* <video

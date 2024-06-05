32d3a6b53809c51d6ad1a787ceb8a9fb324ebc47
Nils Landsberg <111305349+nilslandsberg@users.noreply.github.com>
2024-06-05 10:08:44 -0400

    IndependenceDaySpecials.js pulling menu items from menu-data.js.  Call to server commented out until bug fixed.

--- app/components/IndependenceDaySpecials.js
+++ app/components/IndependenceDaySpecials.js
@@ -1,29 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { renderBulkBbq, renderMenuItems, renderSides } from '../utils/renderMenuItems';
 import { independenceDayDateCheck } from '../utils/dateCheck';
+import { independenceDayItems, independenceDaySides } from '../menu-data';
 
 const IndependenceDaySpecials = () => {
-  const [independenceDayItems, setIndependenceDayItems] = useState([]);
-  const [independenceDaySides, setIndependenceDaySides] = useState([]);
+  // const [independenceDayItems, setIndependenceDayItems] = useState([]);
+  // const [independenceDaySides, setIndependenceDaySides] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchHolidayItems = async () => {
-      try {
-        const response = await fetch('https://u2canque-server.onrender.com/api/holiday-items');
-        const data = await response.json();
-        const independenceDayItems = data.filter((item) => item.type === 'Independence Day');
-        const independenceDaySides = independenceDayItems.filter((item) => item.side);
-        setIndependenceDayItems(independenceDayItems.filter((item) => !item.side));
-        setIndependenceDaySides(independenceDaySides);
-        setIsLoading(false);
-      } catch (error) {
-        console.error('Error fetching Independence Day items:', error);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchHolidayItems = async () => {
+  //     try {
+  //       const response = await fetch('https://u2canque-server.onrender.com/api/holiday-items');
+  //       const data = await response.json();
+  //       const independenceDayItems = data.filter((item) => item.type === 'Independence Day');
+  //       const independenceDaySides = independenceDayItems.filter((item) => item.side);
+  //       setIndependenceDayItems(independenceDayItems.filter((item) => !item.side));
+  //       setIndependenceDaySides(independenceDaySides);
+  //       setIsLoading(false);
+  //     } catch (error) {
+  //       console.error('Error fetching Independence Day items:', error);
+  //     }
+  //   };
 
-    fetchHolidayItems();
-  }, []);
+  //   fetchHolidayItems();
+  // }, []);
 
   // Check to see if the Independence Day Sale is happening
   const independenceDaySale = independenceDayDateCheck();

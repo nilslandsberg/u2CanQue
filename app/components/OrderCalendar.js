aeac01fa80fc16d5851219b191b235081fbac925
Nils Landsberg <111305349+nilslandsberg@users.noreply.github.com>
2024-10-06 23:56:46 -0500

    Update OrderCalendar.js

--- app/components/OrderCalendar.js
+++ app/components/OrderCalendar.js
@@ -57,11 +57,12 @@ const OrderCalendar = () => {
       if (luxonDateDay === tomorrowDay && luxonDateMonth === tomorrowMonth) {
           return true;
       }
+    }
     
     // Disable Specific Date
-      if (year === 2024 && month === 09 && (dayOfMonth === 7 || dayOfMonth === 8 || dayOfMonth === 9 || dayOfMonth === 10 || dayOfMonth === 14 || dayOfMonth === 15 || dayOfMonth === 16 || dayOfMonth === 17 || dayOfMonth === 18)) {
-         return true;
-      }
+    if (year === 2024 && month === 09 && (dayOfMonth === 7 || dayOfMonth === 8 || dayOfMonth === 9 || dayOfMonth === 10 || dayOfMonth === 14 || dayOfMonth === 15 || dayOfMonth === 16 || dayOfMonth === 17 || dayOfMonth === 18)) {
+        return true;
+    }
 
     return false;
   };
